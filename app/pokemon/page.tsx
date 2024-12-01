7eee0486a1742676c22c0ce27a6bc77814728713
brad preston <bap5393@gmail.com>
2024-11-30 22:32:44 -0500

    Replaced pagination code in page with new pagination component

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+'use client';
 
-import { Card } from "@/components/card";
-import Link from "next/link";
-import useAllPokemon from "@/hooks/useAllPokemon";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { FormEvent, useEffect, useMemo } from "react";
+import { Card } from '@/components/card';
+import Link from 'next/link';
+import useAllPokemon from '@/hooks/useAllPokemon';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { FormEvent, useMemo } from 'react';
+import Pagination from '@/components/pagination';
 
 const POKEMON_PER_PAGE = 20;
 
@@ -15,15 +16,9 @@ export default function AllPokemon() {
     () => new URLSearchParams(searchParams),
     [searchParams]
   );
-  const page = params.get("page");
-  const query = params.get("query");
+  const query = params.get('query');
   const pathname = usePathname();
   const { replace } = useRouter();
-
-  useEffect(() => {
-    if (page && parseInt(page) <= 1) params.delete("page");
-    replace(`${pathname}?${params.toString()}`);
-  }, [page, params, pathname, replace]);
 
   if (isLoading) {
     return <h1>loading...</h1>;
@@ -42,13 +37,13 @@ export default function AllPokemon() {
 
     const params = new URLSearchParams(searchParams);
     const formData = new FormData(e.currentTarget);
-    const query = formData.get("query");
+    const query = formData.get('query');
 
     if (query) {
-      params.set("query", query.toString());
-      params.delete("page");
+      params.set('query', query.toString());
+      params.delete('page');
     } else {
-      params.delete("query");
+      params.delete('query');
     }
 
     replace(`${pathname}?${params.toString()}`);
@@ -56,122 +51,51 @@ export default function AllPokemon() {
 
   function getQueryURLParam() {
     const params = new URLSearchParams(searchParams);
-    return params.get("query") || "";
+    return params.get('query') || '';
   }
 
   function getPageURLParam() {
     const params = new URLSearchParams(searchParams);
-    return parseInt(params.get("page") || "1");
+    return parseInt(params.get('page') || '1');
   }
 
   function pagination() {
     if (!data) return;
-    const totalPages = Math.ceil(
-      data.getAllPokemon.filter(({ species }) => !species.includes("-"))
-        .length / POKEMON_PER_PAGE
-    );
 
     return (
-      <ul className="py-10 justify-center flex gap-4 items-end">
-        {page && parseInt(page) > 1 && (
-          <li>
-            <Link
-              href={`${pathname}?page=${parseInt(page) - 1}`}
-              className="border-2 flex justify-center items-center w-8 h-8"
-            >
-              &#10094;
-            </Link>
-          </li>
-        )}
-        <li>
-          <Link
-            href={`${pathname}?page=1`}
-            className="border-2 flex justify-center items-center w-8 h-8"
-          >
-            1
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`${pathname}?page=2`}
-            className="border-2 flex justify-center items-center w-8 h-8"
-          >
-            2
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`${pathname}?page=3`}
-            className="border-2 flex justify-center items-center w-8 h-8"
-          >
-            3
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`${pathname}?page=4`}
-            className="border-2 flex justify-center items-center w-8 h-8"
-          >
-            4
-          </Link>
-        </li>
-        <li>
-          <p>...</p>
-        </li>
-        <li>
-          <Link
-            href={`${pathname}?page=${totalPages}`}
-            className="border-2 flex justify-center items-center w-8 h-8"
-          >
-            {totalPages}
-          </Link>
-        </li>
-        {page && parseInt(page) < totalPages && (
-          <li>
-            <Link
-              href={`${pathname}?page=${parseInt(page) + 1}`}
-              className="border-2 flex justify-center items-center w-8 h-8"
-            >
-              &#10095;
-            </Link>
-          </li>
-        )}
-        {!page && (
-          <li>
-            <Link
-              href={`${pathname}?page=2`}
-              className="border-2 flex justify-center items-center w-8 h-8"
-            >
-              &#10095;
-            </Link>
-          </li>
-        )}
-      </ul>
+      <div className='flex items-center'>
+        <Pagination
+          list={data.getAllPokemon.filter(
+            ({ species }) => !species.includes('-')
+          )}
+          perPage={POKEMON_PER_PAGE}
+        />
+      </div>
     );
   }
 
   return (
-    <div className="max-w-[1000px] px-8 mx-auto relative">
-      <div className="h-16 sticky top-0 w-full flex justify-center items-center z-50 bg-black bg-opacity-70 backdrop-blur-lg">
+    <div className='max-w-[1000px] px-8 mx-auto relative'>
+      <div className='h-16 fixed top-0 w-full left-0 flex justify-center items-center z-50 bg-white bg-opacity-70 backdrop-blur-lg'>
         <form onSubmit={handleSubmit}>
           <input
-            type="text"
-            className="text-zinc-800 w-56 py-1 px-4 bg-white border-white border-2 rounded-tl-md rounded-bl-md"
-            defaultValue={searchParams.get("query")?.toString()}
-            name="query"
+            type='text'
+            className='text-zinc-800 w-56 py-1 px-4 bg-white border-zinc-800 border-2 border-r-0 rounded-tl-md rounded-bl-md'
+            defaultValue={searchParams.get('query')?.toString()}
+            name='query'
           />
           <input
-            type="submit"
-            value="Search"
-            className="w-fit border-white border-2 py-1 px-8 rounded-tr-md rounded-br-md"
+            type='submit'
+            value='Search'
+            className='w-fit border-zinc-700 border-2 py-1 px-8 text-white bg-zinc-700 rounded-tr-md rounded-br-md cursor-pointer hover:bg-zinc-900 hover:border-zinc-900 transition'
           />
         </form>
       </div>
-      <div className="grid grid-cols-2 gap-8 mb-16 md:grid-cols-4">
+      <div className='grid grid-cols-2 gap-8 my-20 md:grid-cols-4'>
         {data.getAllPokemon
           .filter(
             ({ species }) =>
-              !species.includes("-") && species.includes(getQueryURLParam())
+              !species.includes('-') && species.includes(getQueryURLParam())
           )
           .filter(({ num }) => {
             if (query) return num;
@@ -185,7 +109,7 @@ export default function AllPokemon() {
             </Link>
           ))}
       </div>
-      <div className="h-16 fixed bottom-0 left-0 w-full flex justify-center items-center bg-black bg-opacity-70 z-50 backdrop-blur-lg">
+      <div className='h-16 fixed bottom-0 left-0 w-full flex justify-center items-center bg-white bg-opacity-70 z-50 backdrop-blur-lg'>
         {query ? null : pagination()}
       </div>
     </div>
